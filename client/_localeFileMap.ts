77f631eeb333070d1e5b00fbe2b656c2501aafcb
Jonathan Brose <jonathanbrose@protonmail.com>
2022-03-16 22:31:00 +0100

    setup

--- client/_localeFileMap.ts
+++ client/_localeFileMap.ts
@@ -1,15 +1,15 @@
 import { GDLocaleMap } from '~types/general';
 
 export const localeFileMap: GDLocaleMap = {
-	"ar": "ar-531cbf91.js",
-	"de": "de-08061372.js",
-	"en": "en-275f4d17.js",
-	"es": "es-9e326cfd.js",
-	"fr": "fr-9e525864.js",
-	"hi": "hi-50946462.js",
-	"ja": "ja-9a916444.js",
-	"nl": "nl-693010cf.js",
-	"pt": "pt-0cfc8e73.js",
-	"ta": "ta-73a72931.js",
-	"zh": "zh-007e40bb.js"
+	"ar": "ar-e1313c26.js",
+	"de": "de-82d547f4.js",
+	"en": "en-3aedb86f.js",
+	"es": "es-7ac62778.js",
+	"fr": "fr-5cddf634.js",
+	"hi": "hi-7f84a49b.js",
+	"ja": "ja-414cdda1.js",
+	"nl": "nl-845e83fd.js",
+	"pt": "pt-633a2317.js",
+	"ta": "ta-29e0d010.js",
+	"zh": "zh-06f14c25.js"
 };
